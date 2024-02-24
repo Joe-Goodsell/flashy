45851e3930f98a9ff8433b72c0720bb685d6c220
Joseph Goodsell <45343223+Joe-Goodsell@users.noreply.github.com>
2024-02-24 12:19:24 +1000

    fix syntax errors

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -19,7 +19,7 @@ fi
 
 # DB_USER="${POSTGRES_USER:=josephgoodsell}"
 # DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-# DB_NAME="{$POSTGRES_DB:=newsletter}"
+# DB_NAME="{$POSTGRES_DB:=cards}"
 # DB_PORT="{$POSTGRES_PORT:=5432}"
 
 DB_USER="josephgoodsell"
@@ -27,15 +27,33 @@ DB_PASSWORD="password"
 DB_NAME="cards"
 DB_PORT="5432"
 
-if [[ -z "{SKIP_DOCKER}" ]]; then
+
+# `-z`: if string is null
+if [[ -z "${SKIP_DOCKER}" ]]; then
+    >&2 echo "Creating docker container..."
     docker run \
         -e POSTGRES_USER=${DB_USER} \
         -e POSTGRES_PASSWORD=${DB_PASSWORD} \
         -e POSTGRES_DB=${DB_NAME} \
+        -e PGDATA=/usr/local/var/postgres \
         -p "${DB_PORT}":5432 \
-        -d postgres \
-        postgres -N 1000
+        -d postgres postgres -N 1000
+        # postgres -N 1000
+        # Notes:
+        # Run image `postgres` in detached mode
+        # -d postgres \ 
+        # Execute `postgres -N 1000` inside container; sets max postgres connections to 1000
+        # postgres -N 1000
+    >&2 echo "Done!"
 fi
+
+# Start postgres, retrying if unavailable
+# sets PGPASSWORD env variable to non-interactively input password in psql
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "${DB_NAME}" -c "\q"; do
+    >&2 echo "Postgres sleeping. Retrying..."
+    sleep 1
+done
 
 >&2 echo "Postgres is running on port ${DB_PORT}!"
 
