35e32b52f9bf7a2d902a59c48539c12c4144b94c
Joseph Goodsell <45343223+Joe-Goodsell@users.noreply.github.com>
2024-03-01 13:41:43 +1000

    remove logfile from git

--- src/tui/screens/statusbar.rs
+++ src/tui/screens/statusbar.rs
@@ -1,13 +1,11 @@
 use ratatui::{
-    layout::{Constraint, Direction, Layout}, 
-    style::{Color, Style}, 
-    text::Span, 
-    widgets::{Block, Paragraph, Widget}
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style},
+    text::Span,
+    widgets::{Block, Paragraph, Widget},
 };
 
 use crate::tui::app::Mode;
-
-
 
 // TODO: Am I really instantatiating a whole new instance of a struct for *each frame*?
 #[derive(Default, Debug)]
@@ -17,20 +15,18 @@ pub struct StatusBar {
 
 impl StatusBar {
     pub fn new(mode: Mode) -> Self {
-        Self {
-            mode
-        }
+        Self { mode }
     }
 }
 
 impl Widget for StatusBar {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer) {
-        let layout = Layout::default().direction(Direction::Horizontal).constraints(vec![
-            Constraint::Percentage(50),
-            Constraint::Percentage(50),
-        ]).split(area);
+        let layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)])
+            .split(area);
 
-        let mode_disp  = format!("MODE: {}", self.mode);
+        let mode_disp = format!("MODE: {}", self.mode);
         let color: Color = match self.mode {
             Mode::NORMAL => Color::Green,
             Mode::INSERT => Color::Red,
@@ -40,8 +36,11 @@ impl Widget for StatusBar {
             .block(Block::default())
             .render(layout[0], buf);
 
-        Paragraph::new(Span::styled("LINE 0X/XX | filename.rs  ", Style::new().bg(Color::LightBlue)))
-            .block(Block::default())
-            .render(layout[1], buf);
+        Paragraph::new(Span::styled(
+            "    LINE 0X/XX | filename.rs  ",
+            Style::new().bg(Color::LightBlue),
+        ))
+        .block(Block::default())
+        .render(layout[1], buf);
     }
 }
